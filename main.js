14dea990378116d7f3019a1236f7f6b50f9a2139
Ibok Patience <ibok@uridiumtechnologies.com>
2023-02-05 16:42:37 +0100

    styling profile

--- main.js
+++ main.js
@@ -20,7 +20,9 @@ const username = document.getElementById("username")
 
 const password = document.getElementById("password")
 
+const loader = document.getElementById("loader")
 
+const submitbtn = document.getElementById("submitbtn")
 
 //Maxium strength password
 const strongRegex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
@@ -39,6 +41,10 @@ const usernamevalue = username.value
    password.style.borderColor = "green"
    if (usernamevalue !== "" && usernamevalue !== null && usernamevalue !== undefined) {
        username.style.borderColor = "green"
+
+       toggleElement(loader, "block")
+       toggleElement(submitbtn, "")
+
        fetch('https://dummyjson.com/auth/login', {
                 //methods -> POST,GET,PUT,PATCH,DELETE
                 method: 'POST',
@@ -50,8 +56,23 @@ const usernamevalue = username.value
                 })
             })
                 .then(res => res.json())
-                .then(console.log);
-       
+                .then((data) => {
+                    if (data?.token) {
+                        sessionStorage.setItem("usersDetail", JSON.stringify(data))
+                        window.location.href = "/src/pages/dashboard.html"
+                    } else {
+                        alert("invalid login details")
+                    }
+                    // window.location.reload();
+                })
+                .catch((reason) => {
+                    console.log(reason)
+                })
+
+                setTimeout(() => {
+                    toggleElement(submitbtn, "block")
+                    toggleElement(loader, "")
+                }, 1000);
    } else {
        username.style.borderColor = "red"
    }
@@ -59,3 +80,17 @@ const usernamevalue = username.value
    password.style.borderColor = "red"
     }
 })
+    
+    
+
+const toggleElement = (element, display) => {
+    if (element.style.display !== "none") {
+        element.style.display = "none"
+    } else {
+        element.style.display = display;
+    }
+}
+
+const profilesection = document.querySelector("#profilesection")
+profilesection.innerHTML = "<h1>Hello Dominique</h1>"
+console.log (profilesection)
